ef702403801fc143ccc23b9d4956839c22fa95c5
satvika.s <satvika.s@PENNANT-VSP191.pennant.local>
2023-06-21 19:07:45 +0530

    updated_commit

--- hrms_payroll/src/DAO/PayRollDao.java
+++ hrms_payroll/src/DAO/PayRollDao.java
@@ -6,9 +6,15 @@ public interface PayRollDao {
 
 	double deductions(double basicPay, double healthInsurance, double gratuity, double pf, int unpaidLeave);
 
-	double totalsal(double basicPay, double hra, double ta, double variablePay, double healthInsurance, double gratuity,
+	double totalsal(double basicPay, double fixedpay, double variablePay, double healthInsurance, double gratuity,
 			double pf, int earnedLeave, int unpaidLeave);
 
-	double grossPay(double basicPay, double hra, double ta, double variablePay, int earnedLeave);
+	double grossPay(double basicPay, double fixedpay, double variablePay, int earnedLeave);
+
+	double calCTC();
+
+	double forHRA(double fixedpay);
+
+	double forspecialAllowance(double fixedpay);
 
 }
--- hrms_payroll/src/DAO/PayRollDaoImpl.java
+++ hrms_payroll/src/DAO/PayRollDaoImpl.java
@@ -1,37 +1,56 @@
 package DAO;
 
 public class PayRollDaoImpl implements PayRollDao {
-	// private double basicPay;
-	// private double hra;
-	// private double ta;
-	// private double gratuity;
-	// private double healthInsurance;
-	// private double variablePay;
-	// private double pf;
-	//
-	// private double gp;
-	// private double dd;
-	// private double ts;
+
+	private double gross;
+	private double deductions_sal;
+
 	@Override
-	public double grossPay(double basicPay, double hra, double ta, double variablePay, int earnedLeave) {
+	public double grossPay(double basicPay, double fixedpay, double variablePay, int earnedLeave) {
 		// TODO Auto-generated method stub
-		return basicPay + hra + ta + variablePay + ((basicPay * 0.008) * earnedLeave);
+		this.gross = basicPay + fixedpay + variablePay + ((basicPay * 0.008) * earnedLeave);
+
+		return basicPay + fixedpay + variablePay + ((basicPay * 0.008) * earnedLeave);
 
 	}
 
 	@Override
 	public double deductions(double basicPay, double healthInsurance, double gratuity, double pf, int unpaidLeave) {
 		// TODO Auto-generated method stub
+		this.deductions_sal = gratuity + healthInsurance + pf + ((basicPay * 0.005) * unpaidLeave);
+
 		return gratuity + healthInsurance + pf + ((basicPay * 0.005) * unpaidLeave);
+
 	}
 
 	@Override
-	public double totalsal(double basicPay, double hra, double ta, double variablePay, double healthInsurance,
+	public double totalsal(double basicPay, double fixedpay, double variablePay, double healthInsurance,
 			double gratuity, double pf, int earnedLeave, int unpaidLeave) {
 		// TODO Auto-generated method stub
-		return grossPay(basicPay, hra, ta, variablePay, earnedLeave)
+		return grossPay(basicPay, fixedpay, variablePay, earnedLeave)
 				- deductions(basicPay, healthInsurance, gratuity, pf, unpaidLeave);
 
+	}
+
+	@Override
+	public double calCTC() {
+		// TODO Auto-generated method stub
+		double ctc = (gross + deductions_sal) * 12;
+		System.out.println(ctc);
+
+		return ctc;
+	}
+
+	@Override
+	public double forHRA(double fixedpay) {
+		// TODO Auto-generated method stub
+		return fixedpay * 0.4;
+	}
+
+	@Override
+	public double forspecialAllowance(double fixedpay) {
+		// TODO Auto-generated method stub
+		return fixedpay * 0.6;
 	}
 
 }
